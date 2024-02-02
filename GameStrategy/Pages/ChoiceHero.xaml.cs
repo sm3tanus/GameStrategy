f2d6534f0e2013e0447d7e542f2b64b97bb6b76c
Flectere <smetanus3@gmail.com>
2024-02-02 11:03:18 +0300

    start layout

---
+++ GameStrategy/Pages/ChoiceHero.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameStrategy.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ChoiceHero.xaml
+    /// </summary>
+    public partial class ChoiceHero : Page
+    {
+        public ChoiceHero()
+        {
+            InitializeComponent();
+        }
+    }
+}
